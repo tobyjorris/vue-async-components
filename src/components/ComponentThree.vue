bd492633738cd276fb05258c2390bd3701121470
Toby Jorris <tobyjorris@gmail.com>
2022-02-07 15:11:08 -0600

    first commit

---
+++ src/components/ComponentThree.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <h1>Component 3</h1>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ComponentThree"
+}
+</script>
+
+<style scoped>
+.container {
+  border: 2px solid orange;
+  height: 350px;
+
+}
+</style>
